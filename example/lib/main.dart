3c26924cb701b14d60934e4236af00c4643d0c67
Malay1121 <dhara9277@gmail.com>
2023-08-20 16:10:23 +0530

    Added example file

---
+++ example/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_helper/responsive_helper.dart';
+
+class ResponsiveExample extends StatefulWidget {
+  const ResponsiveExample({super.key});
+
+  @override
+  State<ResponsiveExample> createState() => _ResponsiveExampleState();
+}
+
+class _ResponsiveExampleState extends State<ResponsiveExample> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    initializeSize(396, 812);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                print(800.h(context));
+                print(MediaQuery.of(context).size.height);
+              },
+              child: Container(
+                width: 390.w(context),
+                height: 800.h(context),
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
